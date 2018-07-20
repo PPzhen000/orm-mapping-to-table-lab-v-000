d6d0cb29aa24a4f6f51264fbccdbfdf603cb4e5e
Ariel Zhen <zhenpingping@live.com>
2018-07-20 21:41:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,23 +7,30 @@ class Student
 
   def initialize(name, grade)
     @name = name
-    @grade = grade 
+    @grade = grade
   end
 
-  def self.create_table 
-
-  end 
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT 
+      )
+      SQL 
+    DB[:conn].execute(sql) 
+  end
 
   def self.drop_table
 
-  end 
+  end
 
-  def self.create 
+  def self.create
 
-  end 
+  end
 
-  def save 
+  def save
 
-  end 
+  end
   
 end
