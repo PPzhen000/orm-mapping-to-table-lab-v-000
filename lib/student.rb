93019b9d4f078a13104dc8b81bd0e228ad4c399d
Ariel Zhen <zhenpingping@live.com>
2018-07-20 21:39:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,29 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade)
+    @name = name
+    @grade = grade 
+  end
+
+  def self.create_table 
+
+  end 
+
+  def self.drop_table
+
+  end 
+
+  def self.create 
+
+  end 
+
+  def save 
+
+  end 
   
 end
