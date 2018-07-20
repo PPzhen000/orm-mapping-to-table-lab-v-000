a05d3cfcadeaabc8d606765e4e2daae8706a4f85
Ariel Zhen <zhenpingping@live.com>
2018-07-20 21:50:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?, ?)
       SQL
-    DB[:conn].execute(sql, self.name, self.grade) 
+    DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT id() FROM students")[0][0] 
   end
 
